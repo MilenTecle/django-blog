a8b3ecee41fbbc4101705f3a86e8ba2b2b7f07f8
Milen Tecle <m_tecle@hotmail.com>
2023-12-23 15:03:15 +0000

    Add a method to the comment model

--- codestar/settings.py
+++ codestar/settings.py
@@ -93,7 +93,8 @@ DATABASES = {
 
 CSRF_TRUSTED_ORIGINS = [
     "https://*.codeanyapp.com",
-    "https://*.herokuapp.com"
+    "https://*.herokuapp.com",
+    "https://8000-milentecle-djangoblog-gkvoa5ynbau.ws-eu107.gitpod.io",
 ]
 
 
