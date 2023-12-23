76478676c472d78c960ea07577d75f84cf08f9fa
Milen Tecle <m_tecle@hotmail.com>
2023-12-23 14:23:16 +0000

    Update the url and admin.py

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-milentecle-djangoblog-y6kkgpqut36.ws-eu107.gitpod.io',
+ALLOWED_HOSTS = ['8000-milentecle-djangoblog-gkvoa5ynbau.ws-eu107.gitpod.io',
 '.herokuapp.com']
 
 
