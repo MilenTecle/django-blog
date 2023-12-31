fe877d178f9547e780a3710790b26fb2db1366b2
Milen Tecle <m_tecle@hotmail.com>
2023-12-31 13:29:50 +0000

    Create the about app

--- codestar/settings.py
+++ codestar/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-milentecle-djangoblog-mg08yznoh2g.ws-eu107.gitpod.io',]
+ALLOWED_HOSTS = ['8000-milentecle-djangoblog-s40i81i7ffq.ws-eu107.gitpod.io',]
 
 
 # Application definition
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog',
     'django_summernote',
+    'about',
 ]
 
 MIDDLEWARE = [
@@ -96,7 +97,7 @@ DATABASES = {
 CSRF_TRUSTED_ORIGINS = [
     "https://*.codeanyapp.com",
     "https://*.herokuapp.com",
-    "https://8000-milentecle-djangoblog-mg08yznoh2g.ws-eu107.gitpod.io",
+    "https://8000-milentecle-djangoblog-s40i81i7ffq.ws-eu107.gitpod.io",
 ]
 
 
